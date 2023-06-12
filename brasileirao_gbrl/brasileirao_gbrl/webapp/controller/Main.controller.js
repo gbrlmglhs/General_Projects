af34c53240a77388f0ac4c8d0be2a63c3147d9a0
Gabriel Magalhães <130715945+gbrlmglhs@users.noreply.github.com>
2023-06-11 21:41:36 -0300

    update files

--- brasileirao_gbrl/brasileirao_gbrl/webapp/controller/Main.controller.js
+++ brasileirao_gbrl/brasileirao_gbrl/webapp/controller/Main.controller.js
@@ -1,15 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("brasileiraogbrl.controller.Main", {
             onInit: function () {
-                
+                var dadosGerais = {
+                    rodada : "10a",
+                    campeonato : "Brasileirão - MGLHS"
+                };
+
+                var dadosModel = new JSONModel();
+                dadosModel.setData(dadosGerais);
+                var view = this.getView();
+                view.setModel(dadosModel, "ModeloDadosGerais");
             }
         });
     });
